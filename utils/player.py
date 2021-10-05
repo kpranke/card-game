40df9edcecbf03d88ca5c6964a557e6ccf5e011b
Kasia Pranke <kasia.pranke@gmail.com>
2021-10-05 20:25:27 +0200

    update

--- utils/player.py
+++ utils/player.py
@@ -15,50 +15,59 @@ class Player:
 
 
 	def play(self):
-		'''randomly pick a Card in cards.
-#Add the Card to the Player's history.
-#Print: {PLAYER_NAME} {TURN_COUNT} played: {CARD_NUMBER} {CARD_SYMBOL_ICON}.
-#Return the Card.'''
+		'''WRITE A BETTER DESCRIPTION randomly pick a Card in cards.'''
+		shuffle(self.cards)
+		card = self.cards.pop()
+		self.history.append(card)
+		print(f"{self.name} {self.turn_count} played: {card}")
+		return card
 
 class Deck:
 	'''A class to store a deck of cards.'''
 	def __init__(self):
 		''' Initialize a deck of cards'''
-		icon = ["Clubs", "Hearts", "Diamonds", "Spades"]
-		color = ["black", "red"]
-		value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-
 		self.cards = []
 		self.players = []
-		for icon in range(4):  
-			for value in range(13):
-				self.cards.append(Card(color, icon, value))
-	def __str__(self): #fill deck?
+
+	def fill_deck(self):	
+		icons = ["Clubs", "Hearts", "Diamonds", "Spades"]
+		colors = ["black", "red"]
+		values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+		for icon in icons:  
+			for value in values:
+				self.cards.append(Card("color", icon, value))
+
+
+	def __str__(self): 
 		'''Shows a deck of cards'''
-		list_of_cards = []
+		string_of_cards = ''
 		for element in self.cards:
-			list_of_cards.append(element.__str__())
-		return list_of_cards
+			string_of_cards += element.__str__() + " "
+		return string_of_cards
 
 	def shuffle(self):
 		''' Shuffles all cards in the list of cards. '''
-		random.shuffle(self.cards)
-		print("deck shuffled")
+		shuffle(self.cards)
+		print("Deck shuffled")
 
 	def distribute(self, players: list):
 		'''
 		Takes a list of players as a parameter and distributes the cards evenly between all the players.
 		'''  
-	'''while len(self.cards) > 0:
-		for player in players:
-			if len(self.cards) > 0:
-				card = self.cards.pop()
-				print(f"{player['name']} receives the following card:{card.show()}")
-				player['cards'].append(card)'''
+		while len(self.cards) > 0:
+			for player in players:
+				if len(self.cards) > 0:
+					card = self.cards.pop()
+					player.cards.append(card)
+		print('Cards distributed.')
 
 newdeck = Deck()
-print(newdeck)
-deck.shuffle()
-for card in newdeck:
-	print(card)
-print(len(newdeck))	
+newdeck.fill_deck()
+newdeck.shuffle()
+
+player_1 = Player('Ian')
+player_2 = Player('Jon')
+player_3 = Player('Ina')
+newdeck.distribute([player_1, player_2, player_3])
+
+player_1.play()
